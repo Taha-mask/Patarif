e641941fe3dfd87393e0d5b99426095b4f15da4a
soliman_512 <soliman512ali@gmail.com>
2025-07-23 21:18:29 +0300

    finish her and introduction

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from './hero-section/hero-section.component'; 
+import { RouterOutlet } from '@angular/router';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    HeroSectionComponent,
+    RouterOutlet,
+    IntroductionComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
