ae2cb64611a101f126aede064183cd0ed7185ecb
Soliman Ali <soliman512ali@gmail.com>
2025-07-30 19:43:55 +0300

    categories & games

--- src/app/components/pages/home/home-games/home-games.component.ts
+++ src/app/components/pages/home/home-games/home-games.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BackgroundComponent } from "../../../background/background.component";
 import { GameCardComponent } from "./game-card/game-card.component";
+import { SliderGamesHomePageComponent } from "./slider-games-home-page/slider-games-home-page.component";
 
 @Component({
   selector: 'app-home-games',
-  imports: [BackgroundComponent, GameCardComponent],
+  imports: [BackgroundComponent, GameCardComponent, SliderGamesHomePageComponent],
   templateUrl: './home-games.component.html',
   styleUrl: './home-games.component.css'
 })
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -3,14 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { IntroductionComponent } from './introduction/introduction.component';
 import { BackgroundComponent } from '../../background/background.component';
+import { CategoriesComponent } from "./categories/categories.component";
+import { HomeGamesComponent } from './home-games/home-games.component';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeroSectionComponent,
     IntroductionComponent,
-    BackgroundComponent
-  ],
+    BackgroundComponent,
+    CategoriesComponent,
+    HomeGamesComponent,
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
