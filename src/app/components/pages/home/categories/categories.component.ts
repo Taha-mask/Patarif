2477389a45c4aeb31cb5350b97c2a53f5e6ed5c5
soliman ali <soliman512ali@gmail.com>
2025-08-22 17:56:57 +0300

    enhance category card

--- src/app/components/pages/home/categories/categories.component.ts
+++ src/app/components/pages/home/categories/categories.component.ts
@@ -2,19 +2,33 @@ import { Component } from '@angular/core';
 import { CategoryCardComponent } from './category-card/category-card.component';
 import { BackgroundComponent } from "../../../background/background.component";
 import { StarsBackgroundComponent } from "../../../stars-background/stars-background.component";
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-categories',
   imports: [
     CategoryCardComponent,
     BackgroundComponent,
     StarsBackgroundComponent
-],
+  ],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
-  onCardClicked(arg0: string) {
-    throw new Error('Method not implemented.');
+  constructor(private router: Router) { }
+
+  onCardClicked(goto: string) {
+    if (goto == 'learningCorner') {
+      this.router.navigate(['/learning']);
+    }
+    else if (goto == 'gamesCorner') {
+      this.router.navigate(['games']);
+    } else if (goto == 'shoppingCorner') {
+      this.router.navigate(['shop']);
+    } else if (goto == 'storyCorner') {
+      // this.router.navigate(['']);
+Swal.fire("this page will be available soon");
+    }
   }
 
 }
