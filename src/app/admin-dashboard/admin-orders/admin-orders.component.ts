a2561360d6ddc4af3fd7daa7fa5ad72afcadcdcc
soliman ali <soliman512ali@gmail.com>
2025-08-30 12:04:10 +0300

    finish orders

--- src/app/admin-dashboard/admin-orders/admin-orders.component.ts
+++ src/app/admin-dashboard/admin-orders/admin-orders.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { AdminNavbarComponent } from "../admin-navbar/admin-navbar.component";
 import { OrderCardComponent } from "./order-card/order-card.component";
+import { SupabaseService } from '../../supabase.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-orders',
-  imports: [AdminNavbarComponent, OrderCardComponent],
+  imports: [AdminNavbarComponent, OrderCardComponent, CommonModule],
   templateUrl: './admin-orders.component.html',
   styleUrl: './admin-orders.component.css'
 })
 export class AdminOrdersComponent {
+  orders: any[] = [];
+  loading = false;
 
+  constructor(private supabaseService: SupabaseService) {}
+
+  async ngOnInit() {
+    await this.loadOrders();
+  }
+
+async loadOrders() {
+  this.loading = true;
+  try {
+    const data = await this.supabaseService.getAllOrders();
+    this.orders = data.filter((order: any) => order.isDelivered === false);
+    
+  } catch (err) {
+    console.error(err);
+  } finally {
+    this.loading = false;
+  }
+}
+
+
+
+  async onDeliverOrder(orderId: string) {
+    await this.supabaseService.markAsDelivered(orderId);
+    await this.loadOrders(); // تحديث القائمة
+  }
 }
--- src/app/admin-dashboard/admin-orders/order-card/order-card.component.ts
+++ src/app/admin-dashboard/admin-orders/order-card/order-card.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import html2canvas from 'html2canvas';
+import { SupabaseService } from '../../../supabase.service';
 
 @Component({
   selector: 'app-order-card',
@@ -8,5 +10,50 @@ import { Component } from '@angular/core';
   styleUrl: './order-card.component.css'
 })
 export class OrderCardComponent {
-  showInvoice: boolean = false;
+  @Input() order: any;
+  @Output() deliver = new EventEmitter<string>();
+
+  markAsDelivered() {
+    this.deliver.emit(this.order.id);
+  }
+
+  orders: any[] = [];
+  selectedOrder: any = null;
+  showLogo = false;
+
+  constructor(private supabaseService: SupabaseService) { }
+
+  async ngOnInit() {
+    this.orders = await this.supabaseService.getAllOrders();
+  }
+
+  async deliverOrder(orderId: string) {
+    await this.supabaseService.markAsDelivered(orderId);
+    this.orders = await this.supabaseService.getAllOrders(); // تحديث بعد التسليم
+    this.selectedOrder = null;
+  }
+  takeScreenshot() {
+    const element = document.getElementById('recipt');
+
+    this.showLogo = true;
+
+    setTimeout(() => {
+      if (element) {
+        html2canvas(element).then((canvas) => {
+          const image = canvas.toDataURL('image/png');
+
+          const link = document.createElement('a');
+          link.href = image;
+          link.download = 'screenshot.png';
+          link.click();
+
+          this.showLogo = false;
+        });
+      }
+    }, 100);
+  }
+
+
+  // with supabase
+
 }
